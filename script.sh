f0f9fc6f0c176b350f1463e7535ba3011c682d97
Andrej Arbanas <andrej.arbanas@q.agency>
2023-07-14 16:25:42 +0200

    Hotfix: script fix

--- script.sh
+++ script.sh
@@ -9,37 +9,9 @@ mv lib/package.json.tmp lib/package.json
 
 # Check if an argument is provided for updating the version
 if [ -n "$1" ]; then
-    # Read the current version from package.json
-    current_version=$(jq -r '.version' lib/package.json)
-
-    # Split the version into major, minor, and patch components
-    IFS='.' read -ra version_components <<< "$current_version"
-    major=${version_components[0]}
-    minor=${version_components[1]}
-    patch=${version_components[2]}
-
-    # Update the version based on the provided argument
-    case "$1" in
-        "major")
-            major=$((major + 1))
-            minor=0
-            patch=0
-            ;;
-        "minor")
-            minor=$((minor + 1))
-            patch=0
-            ;;
-        "patch")
-            patch=$((patch + 1))
-            ;;
-        *)
-            echo "Invalid update argument. Valid arguments are: major, minor, patch."
-            exit 1
-            ;;
-    esac
 
     # Update the version in package.json
-    updated_version="$major.$minor.$patch"
+    updated_version="$1"
     jq --arg version "$updated_version" '.version = $version' lib/package.json > lib/package.json.tmp
     mv lib/package.json.tmp lib/package.json
 
